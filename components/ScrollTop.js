df5eee1ebb86309b051a1e7372d367718fa92dca
th1nhng0 <ngophuthinh2001@gmail.com>
2021-09-11 18:45:47 +0700

    add to top and to comment

--- components/ScrollTop.js
+++ components/ScrollTop.js
@@ -7,7 +7,7 @@ const ScrollTop = () => {
   useEffect(() => {
     smoothscroll.polyfill();
     const handleWindowScroll = () => {
-      if (window.scrollY > 200) setShow(true);
+      if (window.scrollY > 50) setShow(true);
       else setShow(false);
     };
 
@@ -15,33 +15,45 @@ const ScrollTop = () => {
     return () => window.removeEventListener("scroll", handleWindowScroll);
   }, []);
 
-  const handleClick = () => {
+  const handleScrollTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
-
+  const handleScrollToComment = () => {
+    document.getElementById("comment").scrollIntoView();
+  };
   return (
-    <button
-      aria-label="Scroll To Top"
-      type="button"
-      onClick={handleClick}
-      style={{ opacity: show ? 1 : 0 }}
-      className="fixed p-2 text-gray-100 transition-opacity bg-gray-800 rounded-md right-8 bottom-8 hover:bg-gray-700"
-    >
-      <svg
-        className="w-[22px] h-[22px]"
-        fill="none"
-        stroke="currentColor"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
+    <div className="fixed flex-col hidden gap-3 right-8 bottom-8 md:flex">
+      <button
+        aria-label="Scroll To Comment"
+        type="button"
+        onClick={handleScrollToComment}
+        style={{ opacity: show ? 1 : 0 }}
+        className="p-2 text-gray-500 transition-all bg-gray-200 rounded-full dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-300"
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth={2}
-          d="M5 10l7-7m0 0l7 7m-7-7v18"
-        />
-      </svg>
-    </button>
+        <svg className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+          <path
+            fillRule="evenodd"
+            d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <button
+        aria-label="Scroll To Top"
+        type="button"
+        onClick={handleScrollTop}
+        style={{ opacity: show ? 1 : 0 }}
+        className="p-2 text-gray-500 transition-all bg-gray-200 rounded-full dark:text-gray-400 dark:bg-gray-700 dark:hover:bg-gray-600 hover:bg-gray-300"
+      >
+        <svg className="w-5 h-5" viewBox="0 0 20 20" fill="currentColor">
+          <path
+            fillRule="evenodd"
+            d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+    </div>
   );
 };
 
