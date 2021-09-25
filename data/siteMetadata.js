8be3dde27e9569c38893dbd4a2ca10d46dde8af2
th1nhng0 <ngophuthinh2001@gmail.com>
2021-09-25 11:49:36 +0700

    fix bug -,-

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,7 +4,7 @@ const siteMetadata = {
   headerTitle: "Thịnh's Corner",
   description: "Góc nhỏ của Thịnh.",
   language: "en-us",
-  siteUrl: "https://thinhcorner.com/",
+  siteUrl: "https://thinhcorner.com",
   siteRepo: "https://github.com/Th1nhNg0/th1nhng0.vercel.app",
   siteLogo: "/static/images/logo.png",
   image: "/static/images/avatar.png",
--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -45,7 +45,7 @@ const siteMetadata = require("../data/siteMetadata");
     ...prettierConfig,
     parser: "html",
   });
-
   // eslint-disable-next-line no-sync
   fs.writeFileSync("public/sitemap.xml", formatted);
+  console.log("Done");
 })();
