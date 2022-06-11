7682928eddee794d270df686e0cab45641fc876a
th1nhng0 <ngophuthinh2001@gmail.com>
2022-06-12 02:10:14 +0700

    sort blog in tags

--- src/pages/tag/[slug].tsx
+++ src/pages/tag/[slug].tsx
@@ -1,4 +1,6 @@
 import { allPosts, Post } from "contentlayer/generated";
+import { pick } from "contentlayer/utils";
+import moment from "moment";
 import { NextSeo } from "next-seo";
 import React from "react";
 import ListLayout from "src/layouts/ListLayout";
@@ -33,8 +35,9 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: { params: { slug: string } }) {
   const slug = params.slug;
-  const posts = allPosts.filter(
-    (post) => post.draft !== true && post.tags.includes(slug)
-  );
+  const posts = allPosts
+    .filter((post) => post.draft !== true && post.tags.includes(slug))
+    .map((blog) => pick(blog, ["slug", "title", "summary", "date", "tags"]))
+    .sort((a, b) => moment(b.date).diff(moment(a.date)));
   return { props: { posts, tagName: slug } };
 }
