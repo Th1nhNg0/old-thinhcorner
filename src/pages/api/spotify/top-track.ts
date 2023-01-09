590480798ccde3ba420f9f0b4b20c36ea381059c
Th1nhNg0 <ngophuthinh2001@gmail.com>
2023-01-09 19:18:06 +0700

    remove cache '-'

--- src/pages/api/spotify/top-track.ts
+++ src/pages/api/spotify/top-track.ts
@@ -16,10 +16,5 @@ export default async function handler(
     previewUrl: track.preview_url,
   }));
 
-  res.setHeader(
-    "Cache-Control",
-    "public, s-maxage=86400, stale-while-revalidate=43200"
-  );
-
   return res.status(200).json({ tracks });
 }
