caff24027803131f755fa7a8c4c8294769b6d951
Th1nhNg0 <ngophuthinh2001@gmail.com>
2023-05-11 16:58:49 +0700

    update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript,Script } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,13 +9,13 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
-        <Script type="text/javascript">
-    (function(c,l,a,r,i,t,y){
+        <Script id="clarity">
+          {`    (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-    })(window, document, "clarity", "script", "h289s67yfg");
-</Script>
+    })(window, document, "clarity", "script", "h289s67yfg");`}
+        </Script>
       </Head>
       <body>
         <Main />
